014043decbda542cd80cbfe978a8a8836c5e361a
jerry <anytime0626@gmail.com>
2024-04-08 14:57:09 +0900

    7th commit(1st GUI)

--- src/main/java/com/app/rtk_manager/MainApplication.java
+++ src/main/java/com/app/rtk_manager/MainApplication.java
@@ -2,7 +2,9 @@ package com.app.rtk_manager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -10,14 +12,28 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class MainApplication extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("RTK_Manager.fxml"));
+            Parent root = fxmlLoader.load();
+
+            root.setOnMousePressed((MouseEvent event) -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+            root.setOnMouseDragged((MouseEvent event) -> {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            });
+
             stage.initStyle(StageStyle.TRANSPARENT);
-            Scene scene = new Scene(fxmlLoader.load(), 470, 480);
+            Scene scene = new Scene(root, 470, 480);
             scene.setFill(Color.TRANSPARENT);
-            //stage.setTitle("RTK Manager");
             stage.setScene(scene);
             stage.show();
 
@@ -31,5 +47,4 @@ public class MainApplication extends Application {
         launch();
     }
 }
-
 
